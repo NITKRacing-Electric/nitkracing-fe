96e77122f93a4c29e64fe1273d4c219d5bb495b9
Zeyio <118244972+Zeyio@users.noreply.github.com>
2023-06-19 12:43:19 +0530

    Update team.js

--- src/assets/data/team.js
+++ src/assets/data/team.js
@@ -3,40 +3,76 @@
 
 const Team = [
     {
-        memberName:"Web Lead",
+        memberName:"Dhruv Yadav",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
-        heading:"Captain",
-        url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/962px-Person_icon_BLACK-01.svg.png"
+        heading:"Team Captain",
+        url:"https://drive.google.com/open?id=1UQawIjHq9c6GQABuxfslY8YnrDinN4Fz"
     },
     {
-        memberName:"Operations Lead",
+        memberName:"Aditya Bilgundi",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
-        heading:"Captain",
-        url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/962px-Person_icon_BLACK-01.svg.png"
+        heading:"Operations Head",
+        url:"https://drive.google.com/open?id=1eVZAipNpbDEEfIGmLbrwle6-0_0m7wMs"
     },
     {
-        memberName:"Mechanical Lead",
+        memberName:"Prasad Savanur",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
-        heading:"Captain",
-        url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/962px-Person_icon_BLACK-01.svg.png"
+        heading:"Mechanical Lead",
+        url:"https://drive.google.com/open?id=1Yi8jklRLlcL7JUck6JqUEc23HMKFNYrj"
     },
     {
-        memberName:"Electrical Lead",
+        memberName:"Kethan Ajmeera ",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
-        heading:"Captain",
-        url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/962px-Person_icon_BLACK-01.svg.png"
+        heading:"Electrical Lead",
+        url:"https://drive.google.com/open?id=1m5geN6YLQBTJn2ox4eC8jTgx2PvhUOq8"
     },
     {
-        memberName:"Marketing Lead",
+        memberName:"Sanket Mane ",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
-        heading:"Captain",
-        url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/962px-Person_icon_BLACK-01.svg.png"
+        heading:"Marketing Head",
+        url:"https://drive.google.com/open?id=1BLsHe_16H0eVwmfg92tx7O680ZIFhwqe"
     },
     {
-        memberName:"Media Lead",
+        memberName:"Rajeev Bhat ",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
-        heading:"Captain",
-        url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/962px-Person_icon_BLACK-01.svg.png"
+        heading:"Media lead",
+        url:"https://drive.google.com/open?id=1ZMDBVVuuDCe3bDbX2cRUkm3NyIW5mGIP"
+    },
+    {
+        memberName:"Samiran V",
+        info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
+        heading:"Powertrain Lead",
+        url:"https://drive.google.com/open?id=15pYwBvbhZiDHIILKlyFv5u0LYJiYWaFz"
+    },
+    {
+        memberName:"Himanshu Garud",
+        info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
+        heading:"VD Lead",
+        url:"https://drive.google.com/open?id=1F0Ihon0rcJ-zv39H3Tnjo1ddap2dIS7X"
+    },
+    {
+        memberName:"Kshamaa Acharya B ",
+        info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
+        heading:"Powertrain head",
+        url:"https://drive.google.com/open?id=1PmeV3fw4O_16dWnQM6OeTnTe1y9qadpv"
+    },
+    {
+        memberName:"Saee Sholapurkar",
+        info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
+        heading:"Battery Packaging and Integration(BPI) lead ",
+        url:"https://drive.google.com/open?id=1jEOKwzwtXA9l7-l5LZmH4yRRawE8FX8D"
+    },
+    {
+        memberName:"Saurabh maurya",
+        info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
+        heading:"Structures lead",
+        url:"https://drive.google.com/open?id=1MxCme21wjiDSGMTYNiTDVbRHVlYwgjRJ"
+    },
+    {
+        memberName:"Vinamra",
+        info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
+        heading:"D-FaQ Subsystem Head",
+        url:"https://drive.google.com/open?id=1xlmy2j1WkR0KB1rkqzHHlTPeFogdMTSC"
     }
 ]
 

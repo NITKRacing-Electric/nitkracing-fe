56929ce5dab3dd5f8cb6d10f57376670ceff8de1
Zeyio <118244972+Zeyio@users.noreply.github.com>
2023-06-19 10:46:45 +0530

    Update achievements.js

--- src/assets/data/achievements.js
+++ src/assets/data/achievements.js
@@ -1,13 +1,32 @@
 const Achievements = [
     {
             title: 'Formula Bharat 2023',
-            description: 'Winner of the Best Powertrain award at Formula Bharat 2023.'
+            description: 'Winner of the Best Powertrain award at Formula Bharat 2023'
     },
     //TODO: Fill up the achievements in the formula bharat
     {
         title: 'Best Battery Design',
-        description: 'Top 3 in Best Battery Design Award at Formula Bharat 2023.'
+        description: 'Top 3 in Best Battery Design Award at Formula Bharat 2023'
     },
+    {
+        title : 'Best Buisness Plan',
+        description : 'Top 3rd place in Business Plan at Formula Bharat 2023'
+    },
+
+    {
+        title : 'FSEV',
+        description : 'First in Software Intelligence and Integration at FSEV 2021'
+    },
+    
+    {
+        title : 'Best Buisness Plan 21-22',
+        description : '2nd place in Business Plan at Formula Bharat Virtuals 21-22'
+    },
+    
+    {
+        title : 'Formula Bharat Virtuals',
+        description : '4th Overall in Formula Bharat Virtuals 21-22'
+    }
 ]
 
 export default Achievements;

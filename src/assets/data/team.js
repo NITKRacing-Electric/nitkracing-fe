ae753caf0be56761a6fb741a7919f6ea25799366
Ace_09 <118244972+Zeyio@users.noreply.github.com>
2023-10-21 12:38:34 +0530

    Update team.js

--- src/assets/data/team.js
+++ src/assets/data/team.js
@@ -26,43 +26,43 @@ const Team = [
         memberName:"Aditya Bilgundi",
         info:"The Operations Head of NITK Racing Club is a pivotal figure overseeing the logistical and administrative aspects of the team's operations. With meticulous attention to detail, they manage race scheduling, budgeting, and resource allocation, ensuring smooth execution of events. Their strategic planning skills are instrumental in optimizing team performance. They coordinate with suppliers, sponsors, and volunteers, fostering strong partnerships crucial for the club's success. Their leadership ensures efficient teamwork, from pit stops to race-day logistics. The Operations Head's ability to adapt to unforeseen challenges and maintain a high level of organization is essential in maintaining the club's competitive edge, both on and off the track.",
         heading:"Operations Head",
-        url:"src/assets/teammembers/IMG_20220424_011428_553 - Aditya S Bilgundi.jpg"
+        url:"aditya"
     },
     {
         memberName:"Prasad Savanur",
         info:"The Mechanical Lead at NITK Racing Club plays a pivotal role in engineering excellence. With profound expertise, they spearhead the design, development, and maintenance of the club's racing vehicles. Their meticulous attention to detail ensures each component is optimized for performance and safety. They lead a team of dedicated engineers, fostering innovation and problem-solving to overcome technical challenges. The Mechanical Lead's hands-on approach includes fabrication, testing, and fine-tuning, resulting in competitive, race-ready cars. Their ability to coordinate seamlessly with other team members, from aerodynamics to electronics, ensures a well-integrated and high-performing machine. Their dedication to precision engineering is the driving force behind the club's success on the track.",
         heading:"Mechanical Lead",
-        url:"src/assets/teammembers/IMG_4028 - Prasad Savanur.heic"
+        url:"prasad"
     },
     {
         memberName:"Kethan Ajmeera ",
         info:"The Electrical Lead at NITK Racing Club is a key driver of innovation and performance in our racing endeavors. With a deep-rooted understanding of electrical systems, they oversee the design, installation, and maintenance of our vehicle's intricate electrical components. From wiring harnesses to sensors and instrumentation, their meticulous attention ensures robust and reliable electrical systems. They lead a dedicated team of engineers, fostering innovation in energy management, data acquisition, and control systems. The Electrical Lead's expertise is critical in optimizing power distribution, enhancing vehicle efficiency, and facilitating real-time data analysis, ultimately contributing to our competitive edge and success on the motorsport stage.",
         heading:"Electrical Lead",
-        url:"src/assets/teammembers/IMG-20230524-WA0039 - kethan ajmeera.jpg"
+        url:"kethan"
     },
     {
         memberName:"Sanket Mane ",
         info:"The Marketing Head of NITK Racing Club is a dynamic leader responsible for driving the team's brand visibility and securing vital sponsorships. They craft and execute strategic marketing campaigns, utilizing digital channels, events, and promotions to engage fans and attract sponsors. With adept negotiation skills, they establish crucial partnerships that provide financial support for the team's success. The Marketing Head ensures a consistent visual identity through effective branding efforts and manages merchandise sales, contributing to the club's financial stability. Their ability to effectively communicate the team's values and accomplishments solidifies their role as a vital bridge between the club, fans, and sponsors, ultimately bolstering the team's competitive advantage.",
         heading:"Marketing Head",
-        url:"src/assets/teammembers/IMG_20230601_001612 - Sanket Mane.jpg"
+        url:"sanket"
     },
     {
         memberName:"Rajeev Bhat ",
         info:"The Media Lead of NITK Racing Club is the key architect of the team's public image and communication strategy. They orchestrate the club's media presence, overseeing content creation, press releases, and social media management. With an eye for storytelling, they engage fans and build a strong online community. Their adeptness in visual and written communication ensures the team's achievements are showcased effectively. The Media Lead also collaborates with photographers, videographers, and journalists to capture and share the excitement of racing events. Their ability to convey the team's passion and dedication to motorsports plays a crucial role in garnering support and maintaining a strong digital presence for NITK Racing Club.",
         heading:"Media lead",
-        url:"src/assets/teammembers/IMG_6635 - Rajeev Bhat.jpg"
+        url:"rajeev"
     },
     {
         memberName:"Samiran V",
         info:"The Powertrain Lead at NITK Racing Club is a technical virtuoso who spearheads the development and optimization of the racing car's powertrain system. Their expertise in mechanical engineering and powertrain dynamics is instrumental in enhancing vehicle performance. They meticulously design, fabricate, and fine-tune the engine, transmission, and drivetrain components to achieve peak efficiency and power output. Their leadership guides a dedicated team of engineers in research and development efforts, ensuring innovation and reliability. The Powertrain Lead's role is critical in achieving competitive acceleration and speed on the race track, contributing significantly to the club's success in motorsport competitions.",
         heading:"Powertrain Lead",
-        url:"src/assets/teammembers/IMG_4400 Copy_Original - Samiran.jpeg"
+        url:"samiran"
     },
     {
         memberName:"Himanshu Garud",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
         heading:"VD Lead",
-        url:"src/assets/teammembers/HIMANSHU GARUD PHOTO new - Lionel Messi.jpg"
+        url:"himanshu"
     },
     {
         memberName:"Kshamaa Acharya B ",

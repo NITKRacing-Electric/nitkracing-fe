c102232402f9c6e58fb396c989c52e947cf54d24
Ace_09 <118244972+Zeyio@users.noreply.github.com>
2023-10-21 11:56:21 +0530

    Update team.js

--- src/assets/data/team.js
+++ src/assets/data/team.js
@@ -6,67 +6,67 @@ const Team = [
         memberName:"Dhruv Yadav",
         info:"As the team captain of NITK Racing Club, they serve as the driving force behind their success. Their unwavering commitment, deep expertise in motorsports, and leadership skills inspire the team. They oversee rigorous training, vehicle development, and race strategy, ensuring the team performs at their best. Their knowledge of racing techniques, engineering principles, and competition dynamics propels them to podium finishes. Beyond the track, they represent the club's values and forge valuable partnerships, acting as an ambassador for the organization. In sum, the NITK Racing Club's captain is the catalyst for their achievements, both on and off the circuit.",
         heading:"Team Captain",
-        url:"https://drive.google.com/file/d/1UQawIjHq9c6GQABuxfslY8YnrDinN4Fz/view?usp=sharing"
+        url:"src/assets/teammembers/ME_NR23_pic-crpd - Dhruv Yadav (1).jpg"
     },
     {
         memberName:"Aditya Bilgundi",
         info:"The Operations Head of NITK Racing Club is a pivotal figure overseeing the logistical and administrative aspects of the team's operations. With meticulous attention to detail, they manage race scheduling, budgeting, and resource allocation, ensuring smooth execution of events. Their strategic planning skills are instrumental in optimizing team performance. They coordinate with suppliers, sponsors, and volunteers, fostering strong partnerships crucial for the club's success. Their leadership ensures efficient teamwork, from pit stops to race-day logistics. The Operations Head's ability to adapt to unforeseen challenges and maintain a high level of organization is essential in maintaining the club's competitive edge, both on and off the track.",
         heading:"Operations Head",
-        url:"https://drive.google.com/file/d/1eVZAipNpbDEEfIGmLbrwle6-0_0m7wMs/view?usp=sharing"
+        url:"src/assets/teammembers/IMG_20220424_011428_553 - Aditya S Bilgundi.jpg"
     },
     {
         memberName:"Prasad Savanur",
         info:"The Mechanical Lead at NITK Racing Club plays a pivotal role in engineering excellence. With profound expertise, they spearhead the design, development, and maintenance of the club's racing vehicles. Their meticulous attention to detail ensures each component is optimized for performance and safety. They lead a team of dedicated engineers, fostering innovation and problem-solving to overcome technical challenges. The Mechanical Lead's hands-on approach includes fabrication, testing, and fine-tuning, resulting in competitive, race-ready cars. Their ability to coordinate seamlessly with other team members, from aerodynamics to electronics, ensures a well-integrated and high-performing machine. Their dedication to precision engineering is the driving force behind the club's success on the track.",
         heading:"Mechanical Lead",
-        url:"https://drive.google.com/file/d/1Yi8jklRLlcL7JUck6JqUEc23HMKFNYrj/view?usp=sharing"
+        url:"src/assets/teammembers/IMG_4028 - Prasad Savanur.heic"
     },
     {
         memberName:"Kethan Ajmeera ",
         info:"The Electrical Lead at NITK Racing Club is a key driver of innovation and performance in our racing endeavors. With a deep-rooted understanding of electrical systems, they oversee the design, installation, and maintenance of our vehicle's intricate electrical components. From wiring harnesses to sensors and instrumentation, their meticulous attention ensures robust and reliable electrical systems. They lead a dedicated team of engineers, fostering innovation in energy management, data acquisition, and control systems. The Electrical Lead's expertise is critical in optimizing power distribution, enhancing vehicle efficiency, and facilitating real-time data analysis, ultimately contributing to our competitive edge and success on the motorsport stage.",
         heading:"Electrical Lead",
-        url:"https://drive.google.com/file/d/1m5geN6YLQBTJn2ox4eC8jTgx2PvhUOq8/view?usp=sharing"
+        url:"src/assets/teammembers/IMG-20230524-WA0039 - kethan ajmeera.jpg"
     },
     {
         memberName:"Sanket Mane ",
         info:"The Marketing Head of NITK Racing Club is a dynamic leader responsible for driving the team's brand visibility and securing vital sponsorships. They craft and execute strategic marketing campaigns, utilizing digital channels, events, and promotions to engage fans and attract sponsors. With adept negotiation skills, they establish crucial partnerships that provide financial support for the team's success. The Marketing Head ensures a consistent visual identity through effective branding efforts and manages merchandise sales, contributing to the club's financial stability. Their ability to effectively communicate the team's values and accomplishments solidifies their role as a vital bridge between the club, fans, and sponsors, ultimately bolstering the team's competitive advantage.",
         heading:"Marketing Head",
-        url:"https://drive.google.com/file/d/1BLsHe_16H0eVwmfg92tx7O680ZIFhwqe/view?usp=sharing"
+        url:"src/assets/teammembers/IMG_20230601_001612 - Sanket Mane.jpg"
     },
     {
         memberName:"Rajeev Bhat ",
         info:"The Media Lead of NITK Racing Club is the key architect of the team's public image and communication strategy. They orchestrate the club's media presence, overseeing content creation, press releases, and social media management. With an eye for storytelling, they engage fans and build a strong online community. Their adeptness in visual and written communication ensures the team's achievements are showcased effectively. The Media Lead also collaborates with photographers, videographers, and journalists to capture and share the excitement of racing events. Their ability to convey the team's passion and dedication to motorsports plays a crucial role in garnering support and maintaining a strong digital presence for NITK Racing Club.",
         heading:"Media lead",
-        url:"https://drive.google.com/file/d/1ZMDBVVuuDCe3bDbX2cRUkm3NyIW5mGIP/view?usp=sharing"
+        url:"src/assets/teammembers/IMG_6635 - Rajeev Bhat.jpg"
     },
     {
         memberName:"Samiran V",
         info:"The Powertrain Lead at NITK Racing Club is a technical virtuoso who spearheads the development and optimization of the racing car's powertrain system. Their expertise in mechanical engineering and powertrain dynamics is instrumental in enhancing vehicle performance. They meticulously design, fabricate, and fine-tune the engine, transmission, and drivetrain components to achieve peak efficiency and power output. Their leadership guides a dedicated team of engineers in research and development efforts, ensuring innovation and reliability. The Powertrain Lead's role is critical in achieving competitive acceleration and speed on the race track, contributing significantly to the club's success in motorsport competitions.",
         heading:"Powertrain Lead",
-        url:"https://drive.google.com/file/d/15pYwBvbhZiDHIILKlyFv5u0LYJiYWaFz/view?usp=sharing"
+        url:"src/assets/teammembers/IMG_4400 Copy_Original - Samiran.jpeg"
     },
     {
         memberName:"Himanshu Garud",
         info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur ornare tincidunt. Vivamus vel facilisis mauris. Fusce id porta lectus, at efficitur est. Vivamus eget arcu a nulla volutpat auctor sed non arcu. Etiam at mattis mauris. Mauris quis tellus in elit rhoncus pretium.",
         heading:"VD Lead",
-        url:"https://drive.google.com/file/d/1F0Ihon0rcJ-zv39H3Tnjo1ddap2dIS7X/view?usp=sharing"
+        url:"src/assets/teammembers/HIMANSHU GARUD PHOTO new - Lionel Messi.jpg"
     },
     {
         memberName:"Kshamaa Acharya B ",
         info:"As the Powertrain Head of NITK Racing Club, this role is responsible for orchestrating the heart and muscle of our racing machine. With a background steeped in mechanical engineering, their mission is to fine-tune and optimize every aspect of our powertrain system. From the engine to the transmission and drivetrain, their expertise ensures peak performance and efficiency. They lead a dedicated team of engineers, fostering innovation and reliability in our powertrain components. Their meticulous work directly impacts our ability to achieve rapid acceleration and maintain competitive speeds on the race track, making them an indispensable figure in our pursuit of victory and motorsport excellence.",
         heading:"Powertrain head",
-        url:"https://drive.google.com/file/d/1PmeV3fw4O_16dWnQM6OeTnTe1y9qadpv/view?usp=sharing"
+        url:"src/assets/teammembers/PXL_20230216_062713381.PORTRAIT - Kshamaa Acharya.jpg"
     },
     {
         memberName:"Saee Sholapurkar",
         info:"The Battery Packaging and Integration (BPI) Lead at NITK Racing Club is the linchpin of our electric vehicle development. With a profound understanding of electrical engineering and battery technology, they oversee the design and integration of our vehicle's energy storage system. Their role involves meticulous packaging and placement of batteries to optimize weight distribution and safety. They lead a dedicated team of engineers, ensuring seamless integration with other vehicle components. The BPI Lead's expertise is crucial in maximizing our electric vehicle's range, power, and reliability, making them instrumental in our pursuit of innovation and success in the world of sustainable motorsport.",
         heading:"Battery Packaging and Integration(BPI) lead ",
-        url:"https://drive.google.com/file/d/1jEOKwzwtXA9l7-l5LZmH4yRRawE8FX8D/view?usp=sharing"
+        url:"src/assets/teammembers/Saee_photo - Saee Sholapurkar.png"
     },
     {
         memberName:"Saurabh maurya",
         info:"The Structures Lead at NITK Racing Club holds a pivotal role in ensuring the integrity and safety of our racing vehicles. Armed with expertise in structural engineering, they oversee the design, analysis, and fabrication of our vehicle's chassis and other critical components. Their meticulous attention to detail and innovation contribute to achieving optimal weight distribution and rigidity, essential for peak performance. Leading a dedicated team of engineers, they prioritize safety and reliability in every aspect of vehicle structure. The Structures Lead's expertise is instrumental in producing a race-ready chassis, contributing significantly to our competitiveness and success in motorsport competitions.",
         heading:"Structures lead",
-        url:"https://drive.google.com/file/d/1MxCme21wjiDSGMTYNiTDVbRHVlYwgjRJ/view?usp=sharing"
+        url:"src/assets/teammembers/IMG-20230531-WA0014 - Saurabh Maurya.jpg"
     },
     {
         memberName:"Vinamra",
@@ -78,7 +78,7 @@ const Team = [
         memberName:"Aryan Agrawal",
         info:"The Electronics Subsystem Head at NITK Racing Club is a pivotal figure in the team's pursuit of cutting-edge technology and performance. With a profound grasp of electrical engineering, they lead the development and integration of advanced electronic systems in our racing vehicles. This includes everything from engine control units to telemetry systems and safety features. They manage a dedicated team of engineers, fostering innovation and reliability in all electronic components. The Electronics Subsystem Head's expertise is essential for optimizing vehicle performance, data analysis, and safety measures. Their role is instrumental in maintaining our competitive edge and ensuring our vehicles are at the forefront of motorsport technology.",
         heading:"Electronics Subsystem Head",
-        url:"https://drive.google.com/file/d/1kBx3eAgQE_xGmpbD0Q47LzXk8kv6GMQJ/view?usp=sharing"
+        url:"src/assets/teammembers/IMG_20230526_203852 - Aryan Agrawal.jpg"
     }
 ]
 

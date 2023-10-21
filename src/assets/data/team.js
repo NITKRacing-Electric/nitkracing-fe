7b9198cf4afdf18c05114876ab52e55fbf5cf0ae
Ace_09 <118244972+Zeyio@users.noreply.github.com>
2023-10-21 12:33:54 +0530

    Update team.js

--- src/assets/data/team.js
+++ src/assets/data/team.js
@@ -2,7 +2,18 @@
 // Todo: Complete the below data
 
 import dhruv from '../teammembers/ME_NR23_pic-crpd-DhruvYadav(1).jpg'
-import aditya from '
+import aditya from '../teammembers/IMG_20220424_011428_553 - Aditya S Bilgundi.jpg'
+import prasad from '../teammembers/IMG_4028 - Prasad Savanur.heic'
+import kethan from '../teammembers/IMG-20230524-WA0039 - kethan ajmeera.jpg'
+import sanket from '../teammembers/IMG_20230601_001612 - Sanket Mane.jpg'
+import rajeev from '../teammembers/IMG_6635 - Rajeev Bhat.jpg'
+import samiran from '../teammembers/IMG_4400 Copy_Original - Samiran.jpeg'
+import himanshu from '../teammembers/HIMANSHU GARUD PHOTO new - Lionel Messi.jpg'
+import kshamaa from '../teammembers/PXL_20230216_062713381.PORTRAIT - Kshamaa Acharya.jpg'
+import saee from '../teammembers/Saee_photo - Saee Sholapurkar.png'
+import saurabh from '../teammembers/IMG-20230531-WA0014 - Saurabh Maurya.jpg'
+import vinamra from '../teammembers/2020_20230601-145657 - Vinamra Parakh.jpg'
+import aryan from '../teammembers/IMG_20230526_203852 - Aryan Agrawal.jpg'
 
 const Team = [
     {
@@ -57,31 +68,31 @@ const Team = [
         memberName:"Kshamaa Acharya B ",
         info:"As the Powertrain Head of NITK Racing Club, this role is responsible for orchestrating the heart and muscle of our racing machine. With a background steeped in mechanical engineering, their mission is to fine-tune and optimize every aspect of our powertrain system. From the engine to the transmission and drivetrain, their expertise ensures peak performance and efficiency. They lead a dedicated team of engineers, fostering innovation and reliability in our powertrain components. Their meticulous work directly impacts our ability to achieve rapid acceleration and maintain competitive speeds on the race track, making them an indispensable figure in our pursuit of victory and motorsport excellence.",
         heading:"Powertrain head",
-        url:"src/assets/teammembers/PXL_20230216_062713381.PORTRAIT - Kshamaa Acharya.jpg"
+        url:"kshamaa"
     },
     {
         memberName:"Saee Sholapurkar",
         info:"The Battery Packaging and Integration (BPI) Lead at NITK Racing Club is the linchpin of our electric vehicle development. With a profound understanding of electrical engineering and battery technology, they oversee the design and integration of our vehicle's energy storage system. Their role involves meticulous packaging and placement of batteries to optimize weight distribution and safety. They lead a dedicated team of engineers, ensuring seamless integration with other vehicle components. The BPI Lead's expertise is crucial in maximizing our electric vehicle's range, power, and reliability, making them instrumental in our pursuit of innovation and success in the world of sustainable motorsport.",
         heading:"Battery Packaging and Integration(BPI) lead ",
-        url:"src/assets/teammembers/Saee_photo - Saee Sholapurkar.png"
+        url:"saee"
     },
     {
         memberName:"Saurabh maurya",
         info:"The Structures Lead at NITK Racing Club holds a pivotal role in ensuring the integrity and safety of our racing vehicles. Armed with expertise in structural engineering, they oversee the design, analysis, and fabrication of our vehicle's chassis and other critical components. Their meticulous attention to detail and innovation contribute to achieving optimal weight distribution and rigidity, essential for peak performance. Leading a dedicated team of engineers, they prioritize safety and reliability in every aspect of vehicle structure. The Structures Lead's expertise is instrumental in producing a race-ready chassis, contributing significantly to our competitiveness and success in motorsport competitions.",
         heading:"Structures lead",
-        url:"src/assets/teammembers/IMG-20230531-WA0014 - Saurabh Maurya.jpg"
+        url:"saurabh"
     },
     {
         memberName:"Vinamra",
         info:"The D-FaQ (Driver Feedback and Quick Action) Subsystem Head at NITK Racing Club plays a crucial role in enhancing the driver's experience and vehicle performance. With a deep understanding of electronics and data analysis, they lead the development of real-time monitoring and feedback systems. These systems provide valuable insights to the driver, helping optimize driving strategies for better lap times. They oversee the integration of sensors, telemetry, and data logging, ensuring seamless communication between the vehicle and driver. The D-FaQ Subsystem Head's expertise contributes significantly to our ability to make quick, data-driven decisions, ultimately enhancing our competitive edge on the race track.",
         heading:"D-FaQ Subsystem Head",
-        url:"src/assets/teammembers/2020_20230601-145657 - Vinamra Parakh.jpg"
+        url:"vinamra"
     },
     {
         memberName:"Aryan Agrawal",
         info:"The Electronics Subsystem Head at NITK Racing Club is a pivotal figure in the team's pursuit of cutting-edge technology and performance. With a profound grasp of electrical engineering, they lead the development and integration of advanced electronic systems in our racing vehicles. This includes everything from engine control units to telemetry systems and safety features. They manage a dedicated team of engineers, fostering innovation and reliability in all electronic components. The Electronics Subsystem Head's expertise is essential for optimizing vehicle performance, data analysis, and safety measures. Their role is instrumental in maintaining our competitive edge and ensuring our vehicles are at the forefront of motorsport technology.",
         heading:"Electronics Subsystem Head",
-        url:"src/assets/teammembers/IMG_20230526_203852 - Aryan Agrawal.jpg"
+        url:"aryan"
     }
 ]
 
